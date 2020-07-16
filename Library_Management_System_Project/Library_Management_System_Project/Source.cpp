1cc7c4eeba620c6baaab238d426814bbc615e9db
Choudhury <J76358@NGC.AUTHORITY>
2020-07-16 18:50:31 -0400

    Update in main function

--- Library_Management_System_Project/Library_Management_System_Project/Source.cpp
+++ Library_Management_System_Project/Library_Management_System_Project/Source.cpp
@@ -1,7 +1,40 @@
 #include<stdio.h>
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+//******************************************************************************************
+//		THE MAIN FUNCTION OF THE PROGRAM
+//******************************************************************************************
 
 void main()
 {
+	char ch;		// For switch operation
+	//intro()		// Starting screen
+	do
+	{
+		//clrscr();		// For clear the screen
+		cout << "\n\n\n\tMAIN MENU";
+		cout << "\n\n\tPlease Select Your Option (1-4)";
+		ch=_getche();		// header file conio 
+							// getche change to _getche
+		switch (ch)
+		{
+		case 1:
+			cout << "Under";
+			break;
+		case 2:
+			cout << "under";
+			break;
+		case 3:
+			cout << "Under";
+		case 4:
+			exit(0);
+		default:
+			cout << "\a";
+
+		}
+	} while (ch != '4');
 	printf(" New project");
 
 	getchar();
